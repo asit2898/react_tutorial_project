e02cff81daffe292a5e5022a834a610cc5e96e32
asit2898 <51470339+asit2898@users.noreply.github.com>
2020-08-24 23:17:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import Login from './pages/Login';
+import ManagerDash from './pages/ManagerDash';
+
+function App() {
+  return <ManagerDash />;
+}
+
+export default App;
